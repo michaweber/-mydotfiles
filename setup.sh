0c5aa1e5136f5e7eb98fec0ebee1cba82d9ca79b
Michael Weber <michael@diewebers.at>
2020-04-26 17:34:26 +0200

    added brew casks back in

--- setup.sh
+++ setup.sh
@@ -20,23 +20,15 @@ if [ -z "$CMD" ]; then
   exit 1
 fi
   
-echo "======= Installing dotfiles ========"
+
+echo " -> Check for brew and install"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/michaweber/mydotfiles/master/install/brew.sh)"
+
+echo " -> Installing dotfiles" 
 DOTFILES_REP="$HOME/.dotfiles"
 mkdir -p "$DOTFILES_REP"
 cd "$DOTFILES_REP"
  
-# if is_macos $1; then
-#   echo " -> checking for if xcode commandline tools exist"
-#   xcode-select -p 1>/dev/null
-#   ret=$?
-#   if [ $ret -eq 2 ]; then
-#     echo " -> install xcode comandline Tools"
-#     xcode-select --install
-#   fi
-# fi
-
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/michaweber/mydotfiles/master/install/brew.sh)"
-
 if git rev-parse --git-dir > /dev/null 2>&1; then
   echo " -> updating existing repository"
   git pull
@@ -44,9 +36,7 @@ else
   git clone https://github.com/michaweber/mydotfiles.git .
 fi
 
-
-# . "$DOTFILES_REP/install/brew.sh"
-
-# if is_macos $1; then
-#   . "$DOTFILES_REP/install/brew-cask.sh"
-# fi
+echo " -> Installing Apps through brew-cask"
+if is_macos $1; then
+  . "$DOTFILES_REP/install/brew-cask.sh"
+fi
